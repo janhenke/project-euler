ff5b01cb14e0c6197a9b2872d461c77cd1d1cc57
Jan Henke <Jan.Henke@taujhe.de>
2016-01-10 12:57:08 +0100

    Add template file for new problems

---
+++ template.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Solves problem xxx from the Project Euler website"""
+
+
+def solve():
+    """Solve the problem and return the result"""
+    pass
+
+
+if __name__ == '__main__':
+    print(solve())
