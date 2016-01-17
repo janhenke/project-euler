1a786a89bdde0ca03ba859042902947cfe0c4fee
Jan Henke <Jan.Henke@taujhe.de>
2016-01-17 20:15:36 +0100

    Add function for the first n fibonacci numbers

--- common/fibonacci.py
+++ common/fibonacci.py
@@ -2,6 +2,22 @@
 """A module defining fibonacci number related functions."""
 
 
+def first_n_fibonacci_numbers(n):
+    """Return the list of the first 'n' fibonacci numbers.
+
+    :param n: The number of the fibonacci numbers, so that len(result) == n
+    :return: The list of the first fibonacci numbers with length 'n'
+    """
+    result = []
+    count = 0
+    a, b = 0, 1
+    while count < n:
+        result.append(b)
+        count += 1
+        a, b = b, a + b
+    return result
+
+
 def fibonacci_numbers_below(n):
     """Return the list of fibonacci numbers smaller than 'n'.
 
