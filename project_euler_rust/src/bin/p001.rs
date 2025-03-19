2383e5f25d98a6caa532a14bdda0c2d8da7714f0
Jan Henke <mail@janhenke.dev>
2025-03-19 22:56:32 +0100

    Solve problem 001 in Rust

--- project_euler_rust/src/bin/p001.rs
+++ project_euler_rust/src/bin/p001.rs
@@ -1,3 +1,13 @@
 fn main() {
+    let mut sum: u64 = 0;
 
+    for i in 1..1000 {
+        if i % 3 == 0 || i % 5 == 0 {
+            sum += i;
+        }
+    }
+
+    println!("Project Euler problem 001");
+    println!("==========================");
+    println!("Solution: {}", sum);
 }
---
+++ project_euler_rust/src/bin/template.rs
@@ -0,0 +1,5 @@
+fn main() {
+    println!("Project Euler problem ###");
+    println!("==========================");
+    println!("Solution: {}", 0);
+}
